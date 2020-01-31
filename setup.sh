47e8ea5180b04a41c81a9c73cd3b3e5cfe6e9faf
Dan Zaratsian <dtz001@gmail.com>
2020-01-31 11:53:56 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,25 @@
 #Setup (assumes the gcloud and bq SKDs are installed)
 
+# Variables
+export GCP_PROJECT=gaming-demos
+export BIGQUERY_DATASET=
+export BIGQUERY_TABLE=
+export PUBSUB_TOPIC=game-logs
+
 # Install python dependencies
 pip install google-cloud-bigquery==1.12.1
 pip install google-cloud-pubsub==0.41.0
 pip install random-username==1.0.2
 
 # Create BigQuery Table
-bq rm -f -t zproject201807:gaming.gaming_events_stream
-bq mk --table --location=US gaming.gaming_events_stream uid:STRING,game_id:STRING,game_server:STRING,game_type:STRING,game_map:STRING,event_datetime:TIMESTAMP,player:STRING,killed:STRING,weapon:STRING,x_cord:INTEGER,y_cord:INTEGER
+bq rm -f -t $GCP_PROJECT:$BIGQUERY_DATASET.$BIGQUERY_TABLE
+bq mk --table --location=US $BIGQUERY_DATASET.$BIGQUERY_TABLE uid:STRING,game_id:STRING,game_server:STRING,game_type:STRING,game_map:STRING,event_datetime:TIMESTAMP,player:STRING,killed:STRING,weapon:STRING,x_cord:INTEGER,y_cord:INTEGER
 
 # Create PubSub Topic
-gcloud pubsub topics create gaming_events
+gcloud pubsub topics create $PUBSUB_TOPIC
 
 # Create PubSub Subscription
-gcloud pubsub subscriptions create --topic gaming_events gaming_events_sub
+gcloud pubsub subscriptions create --topic $PUBSUB_TOPIC $PUBSUB_TOPIC-sub
 
 # Setup Dataflow Project (PubSub Subscription > BigQuery Streaming)
 # I'm currently doing this manually within the GCP Console, under Dataflow.
